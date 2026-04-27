4d71b5447046c50805049ca17f5a1220539acd29
francesco.docimo <francesco.docimo@avanade,com>
2026-04-27 11:42:43 +0200

    move to main

---
+++ src/test/java/open/shift/demo/hourly/HourlyApplicationTests.java
@@ -0,0 +1,13 @@
+package open.shift.demo.hourly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
