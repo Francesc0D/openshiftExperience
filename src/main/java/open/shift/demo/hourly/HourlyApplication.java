fd43bf1867749bdd43f068cf7c7b7a5d6731499a
francesco.docimo <francesco.docimo@avanade,com>
2026-05-12 17:56:30 +0200

    prova tag

--- src/main/java/open/shift/demo/hourly/HourlyApplication.java
+++ src/main/java/open/shift/demo/hourly/HourlyApplication.java
@@ -9,5 +9,15 @@ public class HourlyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HourlyApplication.class, args);
 	}
+//    @Bean
+    //public OpenAPI customOpenAPI() {
+    //    return new OpenAPI()
+    //            .info(new Info()
+    //                    .title("Hourly API")
+    //                    .version("1.0")
+    //                    .description("API to get the current hour of the server")
+    //                    .contact(new Contact()
+    //                            .name("Support Team")
+    //                            .email("
 
 }
