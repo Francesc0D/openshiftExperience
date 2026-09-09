e00c61f1f182daf9fe26c7eeb464da36cf941c7e
francesco.docimo <francesco.docimo@avanade,com>
2026-09-09 15:54:16 +0200

    versione aggiornata e funzionante

--- src/main/java/open/shift/demo/hourly/thehour/controller/TimeController.java
+++ src/main/java/open/shift/demo/hourly/thehour/controller/TimeController.java
@@ -1,5 +1,6 @@
 package open.shift.demo.hourly.thehour.controller;
 
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
